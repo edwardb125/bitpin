8efdf1597bc1685ede8fcfeeb42ac4604af8da0c
erfan <erfanhematig@gmail.com>
2022-08-28 20:34:24 +0430

    Finish task

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print('hello world')
+from flask import Flask
+from utils import home_page_function, list_function, article_function, add_vote_function
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home_page():
+    return home_page_function()
+
+@app.route('/list', methods=['GET'])
+def list():
+    return list_function()
+
+@app.route('/vote', methods=['GET'])
+def article():
+    return article_function()
+
+@app.route('/add_vote', methods=['POST'])
+def add_vote():
+    add_vote_function()
+    return list()
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug= True)
---
+++ utils.py
@@ -0,0 +1,40 @@
+from xml.dom.minidom import Document
+from settings import contents
+from flask import render_template, request
+
+
+user_score = {
+    "Crypto": -1,
+    "Bitcoin": -1,
+    "Bitpin": -1,
+}
+
+def home_page_function():
+    return 'hello'
+
+
+def list_function():
+    result = contents.find()
+    content = [result, user_score]
+    return render_template('list.html', contents = content)
+    
+
+def article_function():
+    result = contents.find()
+    content = [result, user_score]
+    return render_template('article.html', contents = content)
+
+
+def add_vote_function():
+    global user_score
+    vote = request.form.get("vote")
+    topic_name = request.form.get("title")
+    user_score[topic_name] = int(vote)
+    content = contents.find_one({
+        'title': topic_name
+    })
+    score = ((content['score'] * content['vote']) + int(vote))/(content['vote'] + 1)
+    update_object = {"$set": {}}
+    update_object["$set"]['score'] = round(score, 3)
+    contents.update_one({'title': topic_name}, update_object)
+    return vote
